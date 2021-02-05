9d3e248683c36c4bd6cca4210b190071d655e843
Norden-Tenzin <nordentenzin29@gmail.com>
2021-02-05 18:29:06 -0500

    added a way to translate from arr to maze

--- constants.py
+++ constants.py
@@ -4,3 +4,8 @@ SIZE = 810
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 DARK = (125, 135, 150)
+RED = (255,0,0)
+GREEN = (0,255,0)
+
+#other
+GAMEFILE = "game.txt"
--- main.py
+++ main.py
@@ -2,12 +2,30 @@ import numpy as np
 import random
 import pygame
 
+from helperfunctions import *
 from constants import *
 
 def initialize():
     pygame.init()
     screen = pygame.display.set_mode((SIZE, SIZE))
     return screen
+
+def mazeMaker(dim, p):
+    arr = [['0' for i in range(dim)] for j in range(dim)] 
+    arr[0][0] = "s"
+    arr[dim-1][dim-1] = "g"
+
+    count = 0
+    for row in arr:
+        for i, ele in enumerate(row):
+            if ele != "s" and ele != "g" and int(ele) == 0:
+                if random.randrange(0, 100, 1)/100 < p:
+                    # print("here")
+                    row[i] = "x"
+                    count += 1
+    
+    writeGame(arr)
+    return arr
 
 def drawBoard(dim):
     top = 5
@@ -19,46 +37,51 @@ def drawBoard(dim):
     difft = diff * diffn
     cellSize = int((SIZE-left-difft)/dim)
 
+    arr = readGame()
     board = pygame.Surface((SIZE, SIZE))
     board.fill((DARK))
     
-    # 1x1
     for row in range(0, dim, 1):
         for col in range(0, dim, 1):
             pygame.draw.rect(board, WHITE, (col*cellSize + (col+1)*diff + left, top + row*diff + row*cellSize , cellSize, cellSize))        
 
-
-        # for fb in range(0, 8, 2):
-        #     pygame.draw.rect(board, WHITE, (y*CELLSIZE, fb *
-        #                                     CELLSIZE, CELLSIZE, CELLSIZE))
-        # for fw in range(1, 9, 2):
-        #     pygame.draw.rect(board, WHITE, ((y+1)*CELLSIZE,
-        #                                     fw*CELLSIZE, CELLSIZE, CELLSIZE))
+    for row, line in enumerate(arr):
+        for col, item in enumerate(line):
+            if item == "s" or item == "g":
+                pygame.draw.rect(board, GREEN, (col*cellSize + (col+1)*diff + left, top + row*diff + row*cellSize , cellSize, cellSize)) 
+            elif item == "x":
+                pygame.draw.rect(board, BLACK, (col*cellSize + (col+1)*diff + left, top + row*diff + row*cellSize , cellSize, cellSize))
 
     return board
 
 def draw(dim, cord, blockType):
-    board = pygame.Surface((SIZE, SIZE))
-    if(blockType == "agent"):
-        pygame.draw.rect(board, WHITE, (col*cellSize + (col+1)*diff + left, top + row*diff + row*cellSize , cellSize, cellSize))        
-
-    if(blockType == "block"):
-    if(blockType == "fire"):
-
     # player, block and fire 
     #cord is a tuple
     #example cord 0,1 
+    top = 5
+    left = 5 # left = lr/2 
 
+    # diff is the border width 
+    diff = 2
+    diffn = dim-1
+    difft = diff * diffn
+    cellSize = int((SIZE-left-difft)/dim)
+
+    board = drawBoard(dim)
+
+    if(blockType == "agent"):
+        pygame.draw.rect(board, RED, (cord[1]*cellSize + (cord[1]+1)*diff + left, top + cord[0]*diff + cord[0]*cellSize , cellSize, cellSize))        
+
+    # if(blockType == "block"):
+    # if(blockType == "fire"):
     return board
 
 def main():
     screen = initialize()
-    arr = mazeMaker(10, 0.3)
-    board = drawBoard(10)
+    arr = mazeMaker(100, 0.3)
+    board = drawBoard(100)
     screen.blit(board, board.get_rect())
-
-    changes = draw(10, (1,1), "block")
-    screen.blit(changes, changes.get_rect())
+    
     on = True
     while on:
         for event in pygame.event.get():
@@ -68,24 +91,7 @@ def main():
 
 
 
-def mazeMaker(dim, p):
-    
-    arr = [['0' for i in range(dim)] for j in range(dim)] 
-    arr[0][0] = "s"
-    arr[dim-1][dim-1] = "g"
 
-    count = 0
-    for row in arr:
-        for i, ele in enumerate(row):
-            if ele != "s" and ele != "g" and int(ele) == 0:
-                if random.randrange(0, 100, 1)/100 < p:
-                    # print("here")
-                    row[i] = "X"
-                    count += 1
-    
-    # print(count)
-    # print(np.matrix(arr))
-    return arr
     
 if __name__ == "__main__":
     main()
