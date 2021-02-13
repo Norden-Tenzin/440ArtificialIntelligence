d4a88fdcedd76cefa8cf42cae2bba62a8d0bdf0b
sabackwon <sabackwon@gmail.com>
2021-02-13 01:27:47 -0500

    a* complete

--- Solution.py
+++ Solution.py
@@ -1,11 +1,12 @@
 import queue
+import math
 
 class Solution:
     def __init__(self, board):
         self.board = board
         self.start = (0, 0)
         # need to change end coord 
-        self.end = (2, 2)
+        self.end = (99, 99)
 
     def find_neighbor(self, board, visited, curr):
         x = curr[0]
@@ -19,14 +20,13 @@ class Solution:
         # start position can not be neighbor
         # visited block can not be neighbor
         for (i, j) in potential_neighbor:
-            if  (i >= 0 and i < 3) and (j >= 0 and j < 3):
+            if  (i >= 0 and i < 100) and (j >= 0 and j < 100):
                 if board[i][j] == '0' or board[i][j] == "g":
                     if (i, j) not in visited:
                         result.append((i, j))
         return result
 
     def create_solution(self, backTrack_info):
-        print(backTrack_info)
         result = []
         curr = backTrack_info[self.end]
 
@@ -41,6 +41,7 @@ class Solution:
         stack = queue.LifoQueue()
         visited = []
         backtrack_info = {}
+        result = []
 
         stack.put(self.start)
         visited.append(self.start)
@@ -50,20 +51,23 @@ class Solution:
             neighbor = self.find_neighbor(self.board, visited, curr)
             if curr == self.end:
                 print("dfs end")
-                print("path")
-                print(self.create_solution(backtrack_info))
+                print("path len")
+                result = self.create_solution(backtrack_info)
+                print(len(result))
+                print("\n")
                 break
             
             for child in neighbor:
                 backtrack_info[child] = curr
                 visited.append(child)
                 stack.put(child)
-        return backtrack_info
+        return result
 
     def bfs(self):
         bfs_queue = queue.Queue()
         visited = []
         backtrack_info = {}
+        result = []
 
         bfs_queue.put(self.start)
         visited.append(self.start)
@@ -73,15 +77,52 @@ class Solution:
             neighbor = self.find_neighbor(self.board, visited, curr)
             if curr == self.end:
                 print("bfs end")
-                print("path")
-                print(self.create_solution(backtrack_info))
+                print("path len")
+                result = self.create_solution(backtrack_info)
+                print(len(result))
+                print("\n")
                 break
             
             for child in neighbor:
                 backtrack_info[child] = curr
                 visited.append(child)
                 bfs_queue.put(child)        
-        return backtrack_info
+        return result
     
+    def a_star(self):
+        p_queue = queue.PriorityQueue()
+        visited = []
+        backtrack_info = {}
+        result = []
+        cost_start_curr = {}
 
+        p_queue.put((0, self.start))
+        cost_start_curr[self.start] = 0
+        visited.append(self.start)
 
+        while not p_queue.empty():
+            curr = p_queue.get()
+            curr = curr[1]
+            neighbor = self.find_neighbor(self.board, visited, curr)
+
+            if curr == self.end:
+                print("A* end")
+                print("path len")
+                result = self.create_solution(backtrack_info)
+                print(len(result))
+
+            for child in neighbor:
+                backtrack_info[child] = curr
+                visited.append(child)
+                cost_start_curr[child] = cost_start_curr[curr] + 1
+                p_queue.put((cost_start_curr[child] + self.heuristic(child) ,child))
+
+        return result
+        
+    def heuristic(self, child):
+        curr_x = child[0]
+        curr_y = child[1]
+        end_x = self.end[0]
+        end_y = self.end[1]
+
+        return math.sqrt((end_x - curr_x)**2 + (end_y - curr_y)**2)
