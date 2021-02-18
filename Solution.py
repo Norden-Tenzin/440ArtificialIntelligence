c96110308bf65d13c2f27632ad9aa251fcd9b84d
sabackwon <sabackwon@gmail.com>
2021-02-18 16:36:36 -0500

    add strat3

--- Solution.py
+++ Solution.py
@@ -3,17 +3,19 @@ import math
 from main import*
 
 class Solution:
-    def __init__(self, board, start):
+    def __init__(self, board, start, mode, fireStartLoc):
         self.board = board
         self.start = start
         # need to change end coord 
         self.end = (len(board) - 1, len(board) - 1)
+        self.mode = mode
+        self.fireLoc = fireStartLoc
         # self.start1 = strat1
         # self.start2 = strat2
         # self.start3 = strat3
 
 
-    def find_neighbor(self, board, visited, curr):
+    def find_neighbor(self, board, visited, curr, algo):
         x = curr[0]
         y = curr[1]
         result = []
@@ -26,9 +28,14 @@ class Solution:
         # visited block can not be neighbor
         for (i, j) in potential_neighbor:
             if  (i >= 0 and i < len(board)) and (j >= 0 and j < len(board)):
-                if board[i][j] == '0' or board[i][j] == "g":
-                    if (i, j) not in visited:
-                        result.append((i, j))
+                if algo == "dfs":
+                    if board[i][j] == '0' or board[i][j] == "g":
+                        if (i, j) not in visited:
+                            result.append((i, j))
+                else:
+                    if board[i][j] == '0' or board[i][j] == "g" or board[i][j] == "2":
+                        if (i, j) not in visited:
+                            result.append((i, j))
         # print(result)
         return result
 
@@ -68,7 +75,7 @@ class Solution:
 
         while not stack.empty():
             curr = stack.get()
-            neighbor = self.find_neighbor(self.board, visited, curr)
+            neighbor = self.find_neighbor(self.board, visited, curr, "dfs")
             
             
             for child in neighbor:
@@ -99,7 +106,7 @@ class Solution:
 
         while not bfs_queue.empty():
             curr = bfs_queue.get()
-            neighbor = self.find_neighbor(self.board, visited, curr)
+            neighbor = self.find_neighbor(self.board, visited, curr, "bfs")
             
             for child in neighbor:
                 backtrack_info[child] = curr
@@ -126,6 +133,7 @@ class Solution:
         backtrack_info = {}
         result = []
         cost_start_curr = {}
+        
         # m = main()
         # print("---------board---------")
         # print(self.board)
@@ -137,14 +145,13 @@ class Solution:
         while not p_queue.empty():
             curr = p_queue.get()
             curr = curr[1]
-            neighbor = self.find_neighbor(self.board, visited, curr)
+            neighbor = self.find_neighbor(self.board, visited, curr, "a_star")
 
             for child in neighbor:
                 backtrack_info[child] = curr
                 visited.append(child)
                 cost_start_curr[child] = cost_start_curr[curr] + 1
-                p_queue.put((cost_start_curr[child] + self.heuristic(child) ,child))
-
+                p_queue.put((cost_start_curr[child] + self.heuristic(child, self.fireLoc, self.mode) ,child))
             if curr == self.end:
                 # print("A* end")
                 # print("visited len")
@@ -157,10 +164,17 @@ class Solution:
         # print(backtrack_info)
         return ({}, visited)
         
-    def heuristic(self, child):
+    def heuristic(self, child, fireLoc, mode):
         curr_x = child[0]
         curr_y = child[1]
         end_x = self.end[0]
         end_y = self.end[1]
+        fire_x = fireLoc[0]
+        fire_y = fireLoc[1]
 
+        #print(math.sqrt((fire_x - curr_x)**2 + (fire_y - curr_y)**2))
+        #print(math.sqrt((end_x - curr_x)**2 + (end_y - curr_y)**2))
+        #print("----------------------------------------")
+        if mode == "strat3":
+            return math.sqrt((end_x - curr_x)**2 + (end_y - curr_y)**2) - math.sqrt((fire_x - curr_x)**2 + (fire_y - curr_y)**2)
         return math.sqrt((end_x - curr_x)**2 + (end_y - curr_y)**2)
