f97c3c8a0513d3ef88e525976f00772ad2b86e60
Norden-Tenzin <nordentenzin29@gmail.com>
2021-03-19 00:12:32 -0400

    added ui elements

--- MINESWEEPER/src/main.py
+++ MINESWEEPER/src/main.py
@@ -15,18 +15,20 @@ smallQuestionPath = os.path.join(THIS_FOLDER, './assets/smallquestion.png')
 minePath = os.path.join(THIS_FOLDER, './assets/smallmine.png')
 flagPath = os.path.join(THIS_FOLDER, './assets/smallflag.png')
 
+helper = False
+
 ## initializes pygame, creates and returns a screen
 def initialize():
-    imageInit()
     pygame.init()
     screen = pygame.display.set_mode((SIZE + UI_SPACE, SIZE))
     return screen
 
 def drawBoard(screen, arr):
     screen.fill(DARK)
-    
-    draw_text('Clear', pygame.font.SysFont("ocraextended", 30), (255, 255, 255), screen, SIZE + 108 + 5, 82)
 
+    title = pygame.font.SysFont("ocraextended", 59).render("MINESWEEPER", True, WHITE)
+    screen.blit(title, [SIZE, 10])    
+    
     for row, line in enumerate(arr):
         for col, item in enumerate(line):
             if item == "m":
@@ -70,6 +72,69 @@ def drawBoard(screen, arr):
                     textobj = pygame.font.SysFont("ocraextended", math.ceil(CELLSIZE*0.75)).render(item, True, color)
                     screen.blit(textobj, [((col*CELLSIZE) + (col*DIFF) + SIDES) + math.ceil((CELLSIZE - math.ceil(CELLSIZE*0.50))/2), SIDES + (row*DIFF) + (row*CELLSIZE) + math.ceil((CELLSIZE - math.ceil(CELLSIZE*0.75))/2)])    
 
+def buttonToggle(bc, ac, screen, action=None):
+    global helper
+
+    mouse = pygame.mouse.get_pos()
+    click = pygame.mouse.get_pressed()
+    rect1 = pygame.Rect(SIZE + 5, 80, 284, 50)
+
+    on_button1 = rect1.collidepoint(mouse)
+    if on_button1 and not helper:  
+        pygame.draw.rect(screen, ac, rect1)
+        if click[0] == 1:
+            helper = True
+            pygame.draw.rect(screen, ac, rect1)
+            pygame.time.delay(200)
+
+    elif on_button1 and helper:
+        if click[0] == 1:
+            helper = False
+            pygame.draw.rect(screen, bc, rect1)
+            pygame.time.delay(200)
+
+    elif not on_button1 and helper: 
+        pygame.draw.rect(screen, ac, rect1)
+    else:
+        pygame.draw.rect(screen, bc, rect1)
+
+    helperTxt = pygame.font.SysFont("ocraextended", 35).render("Helper", True, WHITE)
+    screen.blit(helperTxt, [SIZE + 10, 85])    
+
+def button(x, y, w, h, bc, ac, screen, env, action=None):
+    mouse = pygame.mouse.get_pos()
+    click = pygame.mouse.get_pressed()
+
+    rect = pygame.Rect(x, y, w, h)
+    on_button = rect.collidepoint(mouse)
+    if on_button:
+        pygame.draw.rect(screen, ac, rect)
+    else:
+        pygame.draw.rect(screen, bc, rect)
+    if on_button:  
+        if click[0] == 1 and action!= None:
+            isDead = False
+            pathLen = 0
+            visitedLen = 0
+            algoName = ""
+            
+            if action == "resetMaze":
+                env.resetMaze()
+                drawBoard(screen, env.getCurr())
+            elif action == "newMaze":
+                env.newMaze()
+                drawBoard(screen, env.getCurr())
+            pygame.time.delay(100)
+    
+    newTxt = pygame.font.SysFont("ocraextended", 15).render("New", True, WHITE)
+    mazeTxt = pygame.font.SysFont("ocraextended", 15).render("Maze", True, WHITE)
+    screen.blit(newTxt, [SIZE + 5 + 284 + 2 + 2, 90])
+    screen.blit(mazeTxt, [SIZE + 5 + 284 + 2 + 2, 105])    
+
+    resetTxt = pygame.font.SysFont("ocraextended", 15).render("Reset", True, WHITE)
+    screen.blit(resetTxt, [SIZE + 5 + 284 + 2 + 2 + 50 + 1, 90])
+    screen.blit(mazeTxt, [SIZE + 5 + 284 + 2 + 2 + 50 + 1, 105])    
+
 def main():
     screen = initialize()
     env = Environment()
@@ -97,19 +162,26 @@ def main():
                     pos = pygame.mouse.get_pos()
                     if (pos[0] > 2 and pos[0] < SIZE-2) and (pos[1] > 2 and pos[1] < SIZE-2):
                         curr = env.query(pos)
-                    board = drawBoard(screen, env.getCurr())
+                    drawBoard(screen, env.getCurr())
                 if event.button == 3:
                     pos = pygame.mouse.get_pos()
                     if (pos[0] > 2 and pos[0] < SIZE-2) and (pos[1] > 2 and pos[1] < SIZE-2):
                         curr = env.flag(pos)
+                    drawBoard(screen, env.getCurr())
                     
-                    board = drawBoard(screen, env.getCurr())
-               
+        button(SIZE + 5 + 284 + 2, 80, 50, 50, DARKER, LIGHTDARK, screen, env, "newMaze")
+        button(SIZE + 5 + 284 + 50 + 4, 80, 50, 50, DARKER, LIGHTDARK, screen, env, "resetMaze")
+        buttonToggle(DARKER, LIGHTDARK, screen)
+        if helper:
+            pygame.draw.circle(screen, GREEN, (SIZE + 270, 105), 4, 0)
+        else:
+            pygame.draw.circle(screen, RED, (SIZE + 270, 105), 4, 0)
             ## FOR KEYPRESSES
             # if event.type == pygame.KEYDOWN:
             #     if event.key == pygame.K_RIGHT:
             #         ## do next move. 
             #         pass 
+            
         pygame.display.flip()
                 
 if __name__ == "__main__":
